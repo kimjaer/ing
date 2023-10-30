f25ca74bbe5c13c6c68e88e90a47b9ed7e14ca9a
kimjaer <wodus0422@gmail.com>
2023-10-30 15:43:13 +0900

    jqurey배워서 써보쟈..

--- js/web.js
+++ js/web.js
@@ -1,32 +1,28 @@
-$(document).ready(function() {
-  // 화면이 준비되면 실행해라 //
-})
+// $(document).ready(function () {
+//   // 화면이 준비되면 실행해라 //
+//   $("h1 a").html("다지워")
+//   // 2번째 대메뉴 텍스트가 네이버로
+//   $("#gnb > li").eq(2).find(".d1a").html("네이버")
+//   // 3번째 대메뉴의 소메뉴중 첫번째 텍스트가 구글
+//   $("#gnb > li").eq(2).find(".d2ul li").eq("0").find("a").html("구글")
+//   $("#gnb > li:eq(2) li:eq(0) a").html("구글")
+//   // 모든 링크 안되게 처리
+//   $("a").attr("href", "none")
+//   //h1 이미지 logo.jpg로 바꿔라
+//   $("h1 img").attr("src", "logo.jpg")
+//   // 제일 많이 쓰는 속성 togglrClass, addClass, removeClass
+//   $("h1 img").addClass("on")
+//   // css(스타일, 값)
+//   $("body").css("background", "yellow")
+// })
 
-$("#gnb").click(function(){
-
-})
-
-$("#gnb li").eq(0).click(function(){
-
-})
-
-$("a img").click(function(){
-
-})
-
-$("#cart").click(function(){
-
-})
-
-$("#gnb li").click(function(){
-
-})
-
-$("#gnb li").find("a").click(function(){
-
-})
-
-$("#gnb li").eq(0).find("a").click(function(){
+$(document).ready(function(){
+  // 선택자문제
+  // gif 인 이미지 없애줘요
+  // 링크값이 a태그 없애줘요
+  // $('img[src$="gif]').css("display", "none")
+  // $("a[href='']").css("display", "none")
+  
+  // $("a").attr("href", "").css("display","none")
 
 })
-
