5c64173222bc6b9a1b08aecf735c42f01b52c6f7
kimjaer <wodus0422@gmail.com>
2023-10-30 11:46:31 +0900

    웹 기능사 보일러플레이트

---
+++ js/web.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // 화면이 준비되면 실행해라 //
+})
+
+$("#gnb").click(function(){
+
+})
+
+$("#gnb li").eq(0).click(function(){
+
+})
+
+$("a img").click(function(){
+
+})
+
+$("#cart").click(function(){
+
+})
+
+$("#gnb li").click(function(){
+
+})
+
+$("#gnb li").find("a").click(function(){
+
+})
+
+$("#gnb li").eq(0).find("a").click(function(){
+
+})
+
